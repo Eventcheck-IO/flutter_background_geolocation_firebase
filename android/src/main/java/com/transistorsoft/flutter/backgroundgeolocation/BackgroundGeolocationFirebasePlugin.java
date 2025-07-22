5146f2ee883567691774aa1b3f218c33940b9e8e
bailey-ran <bailey@eventcheck.io>
2025-07-22 14:38:13 +0800

    Update BackgroundGeolocationFirebasePlugin.java

--- android/src/main/java/com/transistorsoft/flutter/backgroundgeolocation/BackgroundGeolocationFirebasePlugin.java
+++ android/src/main/java/com/transistorsoft/flutter/backgroundgeolocation/BackgroundGeolocationFirebasePlugin.java
@@ -1,61 +1,62 @@
 package com.transistorsoft.flutter.backgroundgeolocation;
 
+import android.content.Context;
 import androidx.annotation.NonNull;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.BinaryMessenger;
 
-/** BackgroundFetchPlugin */
-public class BackgroundGeolocationFirebasePlugin implements FlutterPlugin, ActivityAware {
+/**
+ * Updated for Android V2 embedding.
+ */
+public class BackgroundGeolocationFirebasePlugin
+    implements FlutterPlugin, ActivityAware {
 
-    // @deprecated Not used by v2.
-    public static void registerWith(Registrar registrar) {
-        BackgroundGeolocationFirebaseModule module = BackgroundGeolocationFirebaseModule.getInstance();
-        module.onAttachedToEngine(registrar.context(), registrar.messenger());
-        if (registrar.activity() != null) {
-            module.setActivity(registrar.activity());
-        }
-    }
+  private Context applicationContext;
+  private BinaryMessenger messenger;
 
-    // @deprecated Called by Application#onCreate
-    public static void setPluginRegistrant(PluginRegistry.PluginRegistrantCallback callback) {
+  // Called when the plugin is attached to the Flutter engine.
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    this.applicationContext = binding.getApplicationContext();
+    this.messenger = binding.getBinaryMessenger();
+    BackgroundGeolocationFirebaseModule.getInstance()
+        .onAttachedToEngine(applicationContext, messenger);
+  }
 
-    }
+  // Called when the plugin is detached from the Flutter engine.
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    BackgroundGeolocationFirebaseModule.getInstance()
+        .onDetachedFromEngine();
+    this.messenger = null;
+    this.applicationContext = null;
+  }
 
-    public BackgroundGeolocationFirebasePlugin() { }
+  // ActivityAware callbacks for when an Activity is available:
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    BackgroundGeolocationFirebaseModule.getInstance()
+        .setActivity(binding.getActivity());
+  }
 
-    @Override
-    public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
-        BackgroundGeolocationFirebaseModule.getInstance().onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
-    }
+  @Override
+  public void onReattachedToActivityForConfigChanges(
+      @NonNull ActivityPluginBinding binding) {
+    BackgroundGeolocationFirebaseModule.getInstance()
+        .setActivity(binding.getActivity());
+  }
 
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        BackgroundGeolocationFirebaseModule.getInstance().onDetachedFromEngine();
-    }
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    // No-op
+  }
 
-    @Override
-    public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
-        BackgroundGeolocationFirebaseModule.getInstance().setActivity(activityPluginBinding.getActivity());
-    }
-
-    @Override
-    public void onDetachedFromActivityForConfigChanges() {
-        // TODO: the Activity your plugin was attached to was
-        // destroyed to change configuration.
-        // This call will be followed by onReattachedToActivityForConfigChanges().
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
-        // TODO: your plugin is now attached to a new Activity
-        // after a configuration change.
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-        BackgroundGeolocationFirebaseModule.getInstance().setActivity(null);
-    }
+  @Override
+  public void onDetachedFromActivity() {
+    BackgroundGeolocationFirebaseModule.getInstance()
+        .setActivity(null);
+  }
 }
